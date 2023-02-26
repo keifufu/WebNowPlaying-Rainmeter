417a268bceaf2028b0d4c7808133508be0f47c84
keifufu <keifufu@noonly.net>
2023-02-26 15:32:41 +0100

    initial commit

---
+++ AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCopyright("Copyright © keifufu & Trevor Hamilton 2023")]
+[assembly: AssemblyVersion("1.0.0")]
+
+// Do not change the entries below!
+#if X64
+[assembly: AssemblyInformationalVersion("3.0.2.2161 (64-bit)")]
+#else
+[assembly: AssemblyInformationalVersion("3.0.2.2161 (32-bit)")]
+#endif
+[assembly: AssemblyProduct("Rainmeter")]
