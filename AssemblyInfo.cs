4d3c832fb5ef04a7f39abc1a51a999ba605ca6d0
keifufu <github@keifufu.dev>
2023-04-20 18:16:42 +0200

    1.1.7

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyCopyright("© 2023 - keifufu, Trevor Hamilton")]
-[assembly: AssemblyVersion("1.1.6")]
+[assembly: AssemblyVersion("1.1.7")]
 
 // Do not change the entries below!
 #if X64
