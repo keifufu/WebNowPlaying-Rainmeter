111d9f3c9c69e6cdf5d28babb6f555faf7052741
keifufu <github@keifufu.dev>
2023-06-18 20:55:29 +0200

    2.0.3

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyCopyright("© 2023 - keifufu, Trevor Hamilton")]
-[assembly: AssemblyVersion("2.0.2")]
+[assembly: AssemblyVersion("2.0.3")]
 
 // Do not change the entries below!
 #if X64
